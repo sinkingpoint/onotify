71d5126574287d488ea9973217a421fab1a5f3c4
sinkingpoint <colin@quirl.co.nz>
2025-03-09 15:03:37 +0000

    flesh out silences view a bit
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

---
+++ src/components/SilenceCard/index.tsx
@@ -0,0 +1,35 @@
+import { HTMLAttributes } from "preact/compat";
+import { GettableSilence } from "../../pkg/types/api";
+import { MatcherCard } from "../MatcherCard";
+
+interface SilenceCardProps extends HTMLAttributes<HTMLSpanElement> {
+	silence: GettableSilence;
+}
+
+export const SilenceCard = ({ silence, ...props }: SilenceCardProps) => {
+	const startedAt = (silence.startsAt > Date.now() ? "Starts" : "Started") + " at: ";
+	const endedAt = (silence.endsAt > Date.now() ? "Ends" : "Ended") + " at: ";
+
+	return (
+		<span {...props} class={"flex flex-col " + (props.class ? props.class : "")}>
+			<span>
+				<a class="font-bold" href={`/silences/${silence.id}`}>
+					ID: {silence.id}
+				</a>
+			</span>
+			<span class="flex flex-row justify-between">
+				<span class="pr-3">
+					<span class="font-bold">{startedAt}</span> {new Date(silence.startsAt).toISOString()}
+				</span>
+				<span>
+					<span class="font-bold">{endedAt}</span> {new Date(silence.endsAt).toISOString()}
+				</span>
+			</span>
+			<span class="flex flex-row">
+				{silence.matchers.map((m) => (
+					<MatcherCard matcher={m} />
+				))}
+			</span>
+		</span>
+	);
+};
--- src/pages/ViewSilences/index.tsx
+++ src/pages/ViewSilences/index.tsx
@@ -1,24 +1,27 @@
-import { useState } from "preact/hooks";
+import { useMemo, useState } from "preact/hooks";
 import Paginator from "../../components/Paginator";
+import { SilenceCard } from "../../components/SilenceCard";
 import { TextBox } from "../../components/TextBox";
+import { getSilences, GetSilencesResponse } from "../../pkg/api/client";
+import { GettableSilenceSpec } from "../../pkg/types/api";
+import { DataPull, useQuery } from "../../pkg/types/utils";
 
 interface ToggleableChitProps {
 	value: string;
-	initialToggled: boolean;
-	onClick?: () => void;
+	toggled: boolean;
+	onClick?: (toggled: boolean) => void;
 }
 
-const TogglableChit = ({ value, initialToggled, onClick }: ToggleableChitProps) => {
-	const [toggled, setToggled] = useState(initialToggled);
+const TogglableChit = ({ value, toggled, onClick }: ToggleableChitProps) => {
 	let className = "cursor-pointer select-none rounded-xl bg-slate-900	px-3 py-1";
 	if (!toggled) {
 		className += " line-through decoration-2";
 	}
 
 	const handleClick = () => {
-		setToggled(!toggled);
+		const newToggled = !toggled;
 		if (onClick) {
-			onClick();
+			onClick(newToggled);
 		}
 	};
 
@@ -29,21 +32,71 @@ const TogglableChit = ({ value, initialToggled, onClick }: ToggleableChitProps) 
 	);
 };
 
-export default () => {
-	const [currentPage, setCurrentPage] = useState(1);
+const getSilencePage = (query: DataPull<GetSilencesResponse, unknown>) => {
+	if (query.state === "pending") {
+		return <div>Loading...</div>;
+	}
+
+	if (query.state === "error") {
+		return <div>Error: {query.error}</div>;
+	}
+
+	const silences = query.result;
+	if (silences.length === 0) {
+		return <div>No silences found</div>;
+	}
+
 	return (
-		<div class="flex flex-col">
-			<h1>Silences</h1>
-			<div>
-				<TextBox placeholder="Filter" />
-			</div>
-			<div>
-				<TogglableChit value="Active Silences" initialToggled={true} />
-				<TogglableChit value="Expired Silences" initialToggled={false} />
-			</div>
-			<div>
-				<Paginator totalPages={100}></Paginator>
-			</div>
+		<div>
+			{silences.map((s) => (
+				<SilenceCard class="pb-3" silence={GettableSilenceSpec.parse(s)} />
+			))}
 		</div>
+	);
+};
+
+export default () => {
+	const DEFAULT_PAGE_SIZE = 10;
+	const [currentPage, setCurrentPage] = useState(1);
+	const [active, setActive] = useState(true);
+	const [expired, setExpired] = useState(false);
+	const silences = useQuery(() => {
+		return getSilences({
+			query: {
+				page: currentPage,
+				limit: DEFAULT_PAGE_SIZE,
+				sort: ["startsAt:desc"],
+				active: active,
+				expired: expired,
+			},
+		});
+	}, [currentPage, active, expired]);
+
+	const numPages = useMemo(() => {
+		if (silences.state !== "success") {
+			return 1;
+		}
+
+		const numSilences = Math.max(parseInt(silences.headers.get("X-Total-Count")), 1);
+
+		return Math.ceil(numSilences / DEFAULT_PAGE_SIZE);
+	}, [silences]);
+
+	return (
+		<span class="flex flex-col">
+			<h1>Silences</h1>
+			<span>
+				<TextBox placeholder="Filter" />
+			</span>
+			<span class="py-2">
+				<TogglableChit value="Active Silences" toggled={active} onClick={(toggled) => setActive(toggled)} />
+				<TogglableChit value="Expired Silences" toggled={expired} onClick={(toggled) => setExpired(toggled)} />
+			</span>
+			<span>
+				<Paginator totalPages={numPages} currentPage={currentPage} setCurrentPage={setCurrentPage} maxPagesInRange={5}>
+					{getSilencePage(silences)}
+				</Paginator>
+			</span>
+		</span>
 	);
 };
