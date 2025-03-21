8fd4553e28ea977c7b84aeadb4d62fca2992d327
sinkingpoint <colin@quirl.co.nz>
2025-03-21 13:37:36 +0000

    make sidebar slide less jank
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -64,7 +64,7 @@ const SideBarGroup = ({ title, icon, initialExpanded, children }: SideBarGroupPr
 		<span>
 			<span class="flex flex-row justify-between side-bar-item side-bar-group" onClick={triggerGroupClick}>
 				<span class="pr-4">{icon}</span>
-				<span>{title}</span>
+				<span class="whitespace-nowrap">{title}</span>
 				<span class="flex-grow" />
 				<span class="side-bar-chevron py-3" onClick={toggleExpanded}>
 					<ChevronUpIcon class={chevronClasses} />
@@ -86,7 +86,7 @@ const SideBarItem = ({ title, href, icon, ...props }: SideBarItemProps) => {
 	return (
 		<a {...props} class={classes + (!icon ? " pl-[3.75rem]" : "")} href={href}>
 			{icon && <span class="pr-4">{icon}</span>}
-			<span>{title}</span>
+			<span class="whitespace-nowrap">{title}</span>
 		</a>
 	);
 };
@@ -102,7 +102,7 @@ export const SideBar = () => {
 
 	if (state === "open") {
 		return (
-			<nav class="flex flex-col rounded-r top-0 sticky side-bar min-w-64" aria-expanded="true">
+			<nav class="flex flex-col rounded-r top-0 sticky side-bar min-w-64 flex-grow-0 basis-0" aria-expanded="true">
 				<div class="p-4 flex flex-row justify-between">
 					<h1>Onotify</h1>
 					<span class="side-bar-chevron flex flex-col justify-center" onClick={() => setState("closed")}>
@@ -120,13 +120,13 @@ export const SideBar = () => {
 				</SideBarGroup>
 
 				<SideBarGroup title="Config" icon={<DocumentTextIcon class="inline size-6" />} initialExpanded={true}>
-					<SideBarItem title="Receivers" href="/config/receivers" />
+					<SideBarItem title="Raw Config" href="/config" />
 				</SideBarGroup>
 			</nav>
 		);
 	} else {
 		return (
-			<nav class="flex flex-col rounded-r top-0 sticky side-bar min-w-16" aria-expanded="false">
+			<nav class="flex flex-col rounded-r top-0 sticky side-bar min-w-16 flex-grow-0 basis-0" aria-expanded="false">
 				<span class="my-5 py-2 self-center side-bar-chevron">
 					<Bars3BottomLeftIcon class="inline size-10" onClick={() => setState("open")} />
 				</span>
