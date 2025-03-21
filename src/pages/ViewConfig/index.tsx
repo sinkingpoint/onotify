6d4a852c6184c24cdc23176a0447aa831b43ef31
sinkingpoint <colin@quirl.co.nz>
2025-03-21 14:03:18 +0000

    initial viewconfig route
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

---
+++ src/pages/ViewConfig/index.tsx
@@ -0,0 +1,23 @@
+import yaml from "js-yaml";
+import { useMemo } from "preact/hooks";
+import { getConfig } from "../../pkg/api/client";
+import { useQuery } from "../../pkg/types/utils";
+
+export default () => {
+	const configPull = useQuery(() => getConfig(), []);
+	const config = useMemo(() => {
+		if (configPull.state === "success") {
+			return yaml.dump(configPull.result);
+		}
+
+		return "";
+	}, [configPull]);
+
+	console.log(configPull);
+	return (
+		<div class="w-full flex flex-col">
+			<h1>Config</h1>
+			<textarea value={config} class="config-input grow mb-5 mr-5 p-3" disabled />
+		</div>
+	);
+};
