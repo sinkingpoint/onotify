206ce9511d5ad6012ab536f2efb54a81b46c96c1
sinkingpoint <colin@quirl.co.nz>
2025-11-12 17:09:26 +0000

    better styling by default for buttons
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- ui/src/components/Button/index.tsx
+++ ui/src/components/Button/index.tsx
@@ -11,7 +11,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button = ({ icon, color, text, ...buttonProps }: ButtonProps) => {
 	buttonProps.class ??= "";
-	buttonProps.class += ` flex flex-row button button-${color}`;
+	buttonProps.class += ` flex flex-row button button-${color} items-center px-4 py-2 rounded-md font-medium hover:opacity-90 disabled:opacity-50 disabled:cursor-not-allowed`;
 
 	return (
 		<button {...buttonProps}>
